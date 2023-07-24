d219bc5b630f3df5539204a5aff0b0725874a679
future0143 <125833563+future0143@users.noreply.github.com>
2023-07-24 21:51:11 +0200

    Add files via upload

---
+++ task7.1.8/src/main/java/org/example/Human.java
@@ -0,0 +1,11 @@
+package org.example;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Human {
+    String firstName;
+    String lastName;
+}
---
+++ task7.1.8/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human human = new Human("Евгения", "Подбуцкая");
+        System.out.println(human);
+    }
+}
